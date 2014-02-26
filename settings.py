86c21d7ea02af712d053d3747289846744e5ffdb
William Oliveira de Lagos <william.lagos@live.com>
2014-02-26 12:46:56 -0300

    Added secret key to heroku deploy

--- settings.py
+++ settings.py
@@ -404,23 +404,23 @@ DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 # These settings are used by the default fabfile.py provided.
 # Check fabfile.py for defaults.
 
-FABRIC = {
-    "SSH_USER": "",  # SSH username
-    "SSH_PASS":  "",  # SSH password (consider key-based authentication)
-    "SSH_KEY_PATH":  "",  # Local path to SSH key file, for key-based auth
-    "HOSTS": [],  # List of hosts to deploy to
-    "VIRTUALENV_HOME":  "",  # Absolute remote path for virtualenvs
-    "PROJECT_NAME": "",  # Unique identifier for project
-    "REQUIREMENTS_PATH": "",  # Path to pip requirements, relative to project
-    "GUNICORN_PORT": 8000,  # Port gunicorn will listen on
-    "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
-    "LIVE_HOSTNAME": "www.example.com",  # Host for public site.
-    "REPO_URL": "",  # Git or Mercurial remote repo URL for the project
-    "DB_PASS": "",  # Live database password
-    "ADMIN_PASS": "",  # Live admin user password
-#    "SECRET_KEY": SECRET_KEY,
-#    "NEVERCACHE_KEY": NEVERCACHE_KEY,
-}
+# FABRIC = {
+#     "SSH_USER": "",  # SSH username
+#     "SSH_PASS":  "",  # SSH password (consider key-based authentication)
+#     "SSH_KEY_PATH":  "",  # Local path to SSH key file, for key-based auth
+#     "HOSTS": [],  # List of hosts to deploy to
+#     "VIRTUALENV_HOME":  "",  # Absolute remote path for virtualenvs
+#     "PROJECT_NAME": "",  # Unique identifier for project
+#     "REQUIREMENTS_PATH": "",  # Path to pip requirements, relative to project
+#     "GUNICORN_PORT": 8000,  # Port gunicorn will listen on
+#     "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
+#     "LIVE_HOSTNAME": "www.example.com",  # Host for public site.
+#     "REPO_URL": "",  # Git or Mercurial remote repo URL for the project
+#     "DB_PASS": "",  # Live database password
+#     "ADMIN_PASS": "",  # Live admin user password
+#     "SECRET_KEY": SECRET_KEY,
+#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
+# }
 
 
 ##################
@@ -433,6 +433,8 @@ FABRIC = {
 try:
     from local_settings import *
 except ImportError:
+    SECRET_KEY = "928f26e4-2b36-48c1-b9b7-5e2049306018df5334a2-3efa-4de8-a10b-010226b85823eaa7584f-0878-43f2-b099-45f4b73519af"
+    NEVERCACHE_KEY = "0085b0ee-8947-4699-ba15-6bbf538cf75f06e504a2-ee28-4c9e-8a23-ffa5bdebc69384f3b8c4-e63f-4c95-874f-d43f59ff92aa"
     pass
 
 
