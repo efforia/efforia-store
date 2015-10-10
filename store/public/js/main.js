ab75b202da45789a8b0b4d91556a7693a564df12
William Oliveira de Lagos <william@efforia.com.br>
2015-10-10 03:59:26 -0300

    Added new assets and finished art on home page

--- store/public/js/main.js
+++ store/public/js/main.js
@@ -3,18 +3,20 @@ function changeLocale(locale) {
   $('#locale').submit();
 }
 
-$(document).ready(function(){
+$(window).load(function(){
   var viewport = $(window).height();
 
   // Footer offsetting for fixed width on Bootstrap
-  var height = $(document).height();
+  /* var height = $("body").height();
+  console.log(height);
+  console.log(viewport);
   var offset = height - viewport;
   if(offset > 0) $('footer').css({'margin-bottom':'0px'});
   else $('footer').css({
     'bottom':'0px',
     'position':'absolute',
     'width':'100%'
-  });
+  }); */
 
   // Jumbotron offsetting on home page based on viewport and jumbotron min-height
   if(viewport >= 720) $('.jumbotron').css({'min-height': $(window).height() - 84});
