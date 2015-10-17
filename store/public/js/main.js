e1afc56f335bd794c9a09d1e9737855e44143f95
William Oliveira de Lagos <william@efforia.com.br>
2015-10-17 04:28:07 -0300

    Finished subnavbar overlay on products

--- store/public/js/main.js
+++ store/public/js/main.js
@@ -1,36 +1,42 @@
+var navbar = 84; /* Navbar actual size */
+var minheight = 450; /* Minimum actual height */
+
 function changeLocale(locale) {
   $('#language').val(locale);
   $('#locale').submit();
 }
 
 function submenuOnTop() {
-  console.log('scroll');
-  if ($(this).scrollTop() >= 84) $('.submenu').css({'top':'0px'});
-  else $('.submenu').css({'top':'84px'});
+  // Put submenu on top after scrolling navbar; returns when scrolls back
+  var submenu = $('.subnavbar');
+  $(document).scroll(function(event){
+    var scrollsTop = $(this).scrollTop();
+    if(scrollsTop >= navbar) submenu.addClass('navbar-fixed-top').css('position','fixed').children().css('top',0);
+    else submenu.removeClass('navbar-fixed-top').css('position','static').children().css('top',84);
+  });
 }
 
-$(document).ready(function(){
-  $('body').scroll(submenuOnTop());
-  $(document).scroll(submenuOnTop());
-})
+function jumbotronOffset() {
+  // Jumbotron offsetting on home page based on viewport and jumbotron min-height
+  var viewport = $(window).height(); /* Viewport height */
+  if(viewport >= minheight) $('.jumbotron').css({'min-height': viewport - navbar});
+}
 
-$(window).load(function(){
-
+function minimumHeight() {
   // Footer offsetting for fixed width on Bootstrap
-  /* var height = $("body").height();
-  console.log(height);
-  console.log(viewport);
+  var height = $("body").height();
   var offset = height - viewport;
   if(offset > 0) $('footer').css({'margin-bottom':'0px'});
   else $('footer').css({
     'bottom':'0px',
     'position':'absolute',
     'width':'100%'
-  }); */
+  });
+}
 
-  // Jumbotron offsetting on home page based on viewport and jumbotron min-height
-  var navbar = 84; /* Navbar actual size */
-  var minheight = 450; /* Minimum actual height */
-  var viewport = $(window).height(); /* Viewport height */
-  if(viewport >= minheight) $('.jumbotron').css({'min-height': viewport - navbar});
-});
+function main() {
+  submenuOnTop();
+  jumbotronOffset();
+}
+
+$(window).load(main);
