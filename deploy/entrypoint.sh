1fc84749cf72dcd3a97e8d195b3a50d3a12d6a8b
William Oliveira de Lagos <william.lagos@outlook.com>
2016-05-12 16:49:12 -0300

    Added more migrations to include shipping models

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -5,5 +5,5 @@ if !(psql -U postgres -h db -lqt | cut -d \| -f 1 | grep -qw efforia); then
 	psql -U postgres -h db -c "create database efforia;" 
 fi
 python manage.py makemigrations --settings="store.production"
-python manage.py migrate --settings="store.production" 
+python manage.py migrate --run-syncdb --settings="store.production" 
 gunicorn store.wsgi -b 0.0.0.0:9000
